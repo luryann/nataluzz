bf5c70dcb34197b58f8d1e8216e1d16e78c7f8b8
Ryan <dev@example.com>
2021-10-21 18:53:44 -0700

    style(init): add device handlers

---
+++ kernel/kernel.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "limine.h"
+#include "video/renderer.h"
+#include "string.h"
+#include "video/video.h"
+#include "init.h"
+#include "memory/mem.h"
+#include "time.h"
+#include "shell.h"
+
+void comout(const char* input);
+
+extern struct limine_module_request module;
+extern struct limine_file* initramfs;
+extern struct limine_file* font;
