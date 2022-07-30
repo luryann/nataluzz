e44f66890455831cb6fed71d2fa2fc621a9f0647
Ryan <dev@example.com>
2022-07-30 13:49:22 -0700

    refactor(kernel): refactor syscalls

---
+++ kernel/video/renderer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../limine.h"
+#include "../string.h"
+#include "../math.h"
+
+extern struct limine_terminal *terminal;
+
+extern uint32_t MouseCursorBuffer[16 * 16];
+extern uint32_t MouseCursorBufferAfter[16 * 16];
+extern bool MouseDrawn;
+
+void printf(const char *format, ...);
+void gPrint(const char chr[], unsigned int xOff, unsigned int yOff, uint32_t Color);
+void putChar(char c);
+void DrawOverlayMouseCursor(uint8_t* mouseCursor, Point position, uint32_t colour);
+void ClearMouseCursor(uint8_t* mouseCursor, Point position);
