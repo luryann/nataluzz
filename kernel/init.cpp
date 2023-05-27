b67095153a3f11d7c1a91f905e2daf85883aff4a
Ryan <dev@example.com>
2023-05-27 15:46:52 -0700

    test(security): refactor device handlers

---
+++ kernel/init.cpp
@@ -0,0 +1,45 @@
+#include "gdt/gdt.h"
+#include "idt/idt.h"
+#include "memory/mem.h"
+#include "idt/interrupts.h"
+#include "kernelDefines.h"
+#include "init.h"
+#include "video/renderer.h"
+#include "memory/malloc.h"
+
+extern "C" void disablePIC();
+
+void init()
+{
+    GDTDescriptor gdtDescriptor;
+    gdtDescriptor.Size = sizeof(GDT) - 1;
+    gdtDescriptor.Offset = (uint64_t)&DefaultGDT;
+    LoadGDT(&gdtDescriptor);
+
+    comout("TEST\n");
+
+    printf("[ %sOK %s] loading GDT\n", Green, White);
+
+    create_idt();
+    InitPS2Mouse();
+
+    printf("[ %sOK %s] loading IDT\n", Green, White);
+
+    physical_kernel_start = kernel_address_request.response->physical_base;
+	virtual_kernel_start = kernel_address_request.response->virtual_base;
+
+    hhdm_offset = hhdm_request.response->offset;
+
+    buffer = framebuffer_request.response->framebuffers[0];  
+    terminal = terminal_request.response->terminals[0];
+
+    initramfs = module.response->modules[0];
+    font = module.response->modules[1];
+
+    printf("[ %sOK %s] buffers filled\n", Green, White);
+
+    parse();
+
+    printf("[ %sOK %s] ramdisk parsed\n", Green, White);
+
+}
