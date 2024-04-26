18f6a46a0269c8ab37d5a0d2df665a341990332d
Ryan <dev@example.com>
2024-04-26 01:18:04 -0700

    refactor(logging): add syscalls

---
+++ kernel/memory/malloc.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stddef.h>
+
+struct kernie_heap
+{
+    void *malloc(size_t size);
+    void *realloc(char* ptr, size_t size);
+    void free(void *ptr);
+
+    bool heap_is_set_up = false;
+    bool have_allocated = false;
+    unsigned char *last_valid_address;
+    unsigned char *heap_space;
+
+    void init(unsigned char* addr);
+
+    static kernie_heap* the(){
+		static kernie_heap me;
+		return &me;
+    }
+};
